519211d8b6244ac775eb192ab8a14bfd8fc4fc64
Matthew Gale <matthewgale359@gmail.com>
2024-08-14 15:20:25 +0100

    redux alert used and displayed on react front-end

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+import Alert from './components/layout/Alert';
 
 // Redux
 //Provider links redux and react together
@@ -22,6 +23,7 @@ const App = () =>
         <Route exact path="/" Component={ Landing }/>
       </Routes>
         <section className='container'>
+          <Alert />
           <Routes>
             <Route exact path="/register" Component={ Register}/>
             <Route exact path="/login" Component={ Login}/>
