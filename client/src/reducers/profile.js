e75da247eda4a06436f07aa8eb9367cdfcf78a80
Matthew Gale <matthewgale359@gmail.com>
2024-08-23 10:26:44 +0100

    removing profile on logout from redux state

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,6 +1,7 @@
 import {
     PROFILE_ERROR,
-    GET_PROFILE
+    GET_PROFILE,
+    CLEAR_PROFILE
 } from '../actions/types';
 
 const initialState = {
@@ -26,6 +27,13 @@ export default function(state = initialState, action) {
             return {
                 ...state,
                 error: payload,
+                loading: false
+            }
+        case CLEAR_PROFILE:
+            return {
+                ...state,
+                profile: null,
+                repos: [],
                 loading: false
             }
         default:
