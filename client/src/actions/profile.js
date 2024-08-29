15b4f2e24cfe6e9812e471be2738b6f9f9a20973
Matthew Gale <matthewgale359@gmail.com>
2024-08-29 11:55:59 +0100

    create profile now works

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { setAlert } from './alert';
 import { GET_PROFILE, PROFILE_ERROR } from './types';
 
 // Get current user's profile
@@ -10,7 +11,46 @@ export const getCurrentUsersProfile = () => async dispatch => {
             payload:res.data
         });
     }catch(err){
+        // console.error(err)
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: {msg: err.response.statusText, status: err.response.status}
+        })
+    }
+}
+
+// Create/Update a profile
+export const createProfile = (formData, navigate, edit = false) => async dispatch => {
+    try {
+        const config = {
+            header: {
+                'Content-Type': 'application/json'
+            }
+        }
+
+        const res = await axios.post('/api/profile', formData, config);
+
+        console.log(res)
+
+        dispatch({
+            type: GET_PROFILE,
+            payload:res.data
+        });
+
+        dispatch(setAlert(edit ? 'Profile Updated' : 'Profile Created'));
+
+        // if creating a new profile, redirect to a new page
+        if (!edit){
+            navigate('/dashboard');
+        }
+    }catch(err){
         console.error(err)
+        const errors = err.response.data.errors;
+
+        if (errors){
+            errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+        }
+
         dispatch({
             type: PROFILE_ERROR,
             payload: {msg: err.response.statusText, status: err.response.status}
--- client/src/components/profile-forms/CreateProfile.js
+++ client/src/components/profile-forms/CreateProfile.js
@@ -1,9 +1,14 @@
 import React, { Fragment, useState } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { createProfile } from '../../actions/profile';
 import Alert from '../layout/Alert';
 
-const CreateProfile = props => {
+const CreateProfile = ({createProfile}) => {
+    
+    const navigate = useNavigate();
+
     const [formData, setFormData] = useState({
         company: '',
         website: '',
@@ -38,6 +43,11 @@ const CreateProfile = props => {
 
     const onChange = e => setFormData({...formData, [e.target.name]: e.target.value});
 
+    const onSubmit = async e => {
+        e.preventDefault();
+        createProfile(formData, navigate)
+    };
+
     return (
         <div className='container'>
             <Alert />
@@ -49,7 +59,7 @@ const CreateProfile = props => {
                 profile stand out
             </p>
             <small>* = required fields</small>
-            <form className="form">
+            <form className="form" onSubmit={e => onSubmit(e)}>
                 <div className="form-group">
                 <select name="status" value={status} onChange={(e) => onChange(e)}>
                     <option value="0">* Select Professional Status</option>
@@ -154,7 +164,8 @@ const CreateProfile = props => {
 }
 
 CreateProfile.propTypes = {
-
+    createProfile: PropTypes.func.isRequired
 }
 
-export default CreateProfile
+{/* no map state to props needed here */}
+export default connect(null, {createProfile})(CreateProfile);
