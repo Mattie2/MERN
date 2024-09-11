c165c6af442c2c079458cee6b5832bb1aa805e0e
Matthew Gale <matthewgale359@gmail.com>
2024-09-11 14:10:19 +0100

    edit profile link added

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -10,7 +10,7 @@ const Profile = ({ getProfileById, profile: {profile, loading}, auth }) => {
     const {id} = useParams()
     useEffect(() => {
         getProfileById(id);
-    }, [getProfileById])
+    }, [getProfileById, id])
 
     return (
         <div className='container'>
@@ -19,6 +19,8 @@ const Profile = ({ getProfileById, profile: {profile, loading}, auth }) => {
                 <Link to='/profiles' className='btn btn-light'>
                     Back To Profiles
                 </Link>
+                {auth.isAuthenticated && auth.loading === false && auth.user._id === profile.user._id && (
+                    <Link to='/edit-profile' className='btn btn-dark'>Edit Profile</Link>)}
             </Fragment>}
         </div>
     )
